f41554d965168505e436e06abc81dc0d4e670fd6
remyacnair <49112727+remyacnair@users.noreply.github.com>
2019-03-31 11:58:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const port = process.env.PORT || 443,
     host = '0.0.0.0', // probably this change is not required
     externalUrl = 'https://sebasuz.herokuapp.com',
-    token = process.env.TOKEN || '704759716:AAGbz6kGY7pE9U65ZmQ3591fNDo1-S_BFak',
+    token = process.env.TOKEN || '704759716:AAGbz6kGY7pE9U65ZmQ3S91fNDo1-S_BFak',
     bot = new TelegramBot(token, {
         webHook: {
             port: port,
